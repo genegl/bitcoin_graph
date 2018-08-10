4b76066447ef567f94c4012b4217f4d9e71114f1
32415 <yevglu@live.com>
2018-08-09 22:48:53 -0400

    Added /analyze/ method. The application is done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bitcoin_graph
-This application shows real Bitcoin prices in US dollars. Written in Python.
+This application shows real Bitcoin prices in US dollars in the last 48 hours. Written in Python.
+You can see results at: https://plot.ly/~zhgzh/0/#/
--- bitcoin_price.py
+++ bitcoin_price.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
+import re
 import urllib2
+import datetime
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
@@ -13,15 +15,50 @@ class GetData():
         pass
 
     def get_real_time(self):
+        """
+        Reading data from API.
+
+        :return: whole dump oof data
+        
+        """
         opener = urllib2.build_opener()
         opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-        response = opener.open('https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD')
+        response = opener.open('https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=48')
         the_answer = response.read()
 
         return the_answer
 
     def analyze_answer(self, what_analyze):
+        """
+        Analyzing recived data.
+
+        :time_index: Gets index for timestamps
+        :real_time: List of date and time in readable format
+        :price_index: Gets index for prices
+        :price_end: End index for price
+        :real_price: List of prices
+        :return: real_price, real_time
+        
+        """
         self.what_analyze = what_analyze
+        real_time = []
+        real_price = []
+        
+        i = 0
+
+        self.time_index = [m.start() for m in re.finditer('"time":', what_analyze)]
+        for xtm in self.time_index:
+            real_time.append(datetime.datetime.fromtimestamp(int(what_analyze[xtm+7:xtm+17])).strftime("%Y-%m-%d %H-%M-%S"))
+
+        self.price_index = [m.start() for m in re.finditer('"close":', what_analyze)]
+        self.price_end = [m.start() for m in re.finditer(',"high":', what_analyze)]
+        for xpr in self.price_index:
+            real_price.append(what_analyze[xpr+8:self.price_end[i]])
+            i += 1
+        
+        return real_price, real_time
+        
+            
 
 
 class DrawGraph():
@@ -29,16 +66,22 @@ class DrawGraph():
     def __init__(self):
         pass
 
-    def do_the_drawing(self, time, price):
+    def do_the_drawing(self, use_data):
+        """
+        Drawing a graph, based on sended data. 'x' represents Time line, 'y' represents Price line
+
+        :use_data: param: List of two items Price and Time
+        
+        """
         price = go.Scatter(
-        x=time,
-        y=price
+        x=use_data[1],
+        y=use_data[0]
         )
-        trace1 = go.Scatter(
-        x=[1, 2, 3, 4],
-        y=[16, 5, 11, 9]
-        )
-        data = [price, trace1]
+##        trace1 = go.Scatter(
+##        x=[1, 2, 3, 4],
+##        y=[16, 5, 11, 9]
+##        )
+        data = [price,] #trace1]
 
         py.plot(data, filename = 'basic-line', auto_open=True)
 
@@ -50,11 +93,7 @@ dump_of_data = the_data.get_real_time()
 clean_data = the_data.analyze_answer(dump_of_data)
 
 the_graph = DrawGraph()
-plot_url = py.plot(the_graph.do_the_drawing())
-print (plot_url)
-#opener = urllib2.build_opener()
-#opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-#response = opener.open('https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD')
-#        the_answer = response.read()
-#print response.read()
-##
+plot_url = py.plot(the_graph.do_the_drawing(clean_data))
+#print (plot_url)
+
+
